d9eb46b53e84894c355c60c50da8f80a9d2a1025
Mike <phegan@gmail.com>
2015-10-13 20:38:18 -0400

    Added the ability to create the arrays via array factory

--- create_monster.py
+++ create_monster.py
@@ -1,9 +1,13 @@
-
+from pathfinder.models.arrays.array_factory import ArrayFactory
 
 
 def main():
     cr = raw_input("Enter CR of monster:")
     print cr
+
+    array_type = raw_input("Enter the array type of monster [combatant, expert, spellcaster]")
+    array = new ArrayFactory(cr, array_type)
+
 
 if __name__ == "__main__":
     main()
