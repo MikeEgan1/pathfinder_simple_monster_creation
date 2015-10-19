b017735c71bb019a4beafdfbec2d026da19ef399
Mike <phegan@gmail.com>
2015-10-18 23:52:08 -0400

    Added a method to create a select string for abilty scores.

--- create_monster.py
+++ create_monster.py
@@ -1,6 +1,18 @@
 from pathfinder.models.arrays.array_factory import ArrayFactory
 from pathfinder.models.monster.monster import Monster
+import json
 
+def get_ability_score_string():
+    with open("json/ability_scores.json") as file:
+        ability_scores = json.load(file)
+
+        ability_score_string = ""
+        count = 1
+
+        for score in ability_scores:
+            ability_score_string = ability_score_string + "\n" + str(count) + ". " + score
+
+        return ability_score_string
 
 def main():
     monster = Monster()
@@ -14,7 +26,9 @@ def main():
 
     monster.applyArray(array)
 
-    high_mod = raw_input("Which Ability Modifier gets +" + str(array.getMasterAbilityMod()) + "\n 1. Strength\n 2. Dexterity \n 3. Constitution\n 4. Intelligence\n 5. Wisdom\n 6. Charisma")
+    ability_scores = get_ability_score_string()
+
+    high_mod = raw_input("Which Ability Modifier gets +" + str(array.getMasterAbilityMod()) + ability_score_string)
 
     great_mod = raw_input("Which Ability Modifier gets +" + str(array.getGreatAbilityMod()) + "\n 1. Strength\n 2. Dexterity \n 3. Constitution\n 4. Intelligence\n 5. Wisdom\n 6. Charisma")
 
