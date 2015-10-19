dae8f9c5ce5a61d4f7ec9ae2861a37e50dc67512
Mike <phegan@gmail.com>
2015-10-18 23:08:59 -0400

    Added getters and setters in the array class, and the monster class

--- models/arrays/monster_array.py
+++ models/arrays/monster_array.py
@@ -28,5 +28,76 @@ class MonsterArray(object):
 
     def getTouchArmorClass(self):
         return self.touch_armor_class
-
+    
+    def setName(self):
+        return self.name 
+    
+    def setXP(self):
+        return self.xp 
+    
+    def setMonsterType(self):
+        return self.monster_type 
+    
+    def setInit(self):
+        return self.init 
+    
+    def setSenses(self):
+        return self.senses 
+    
+    def setPerception(self):
+        return self.perception 
+    
+    def setSize(self):
+        return self.size 
+    
+    def setSpeed(self):
+        return self.speed 
+    
+    def setFlatFootedArmorClass(self):
+        return self.flat_footed_armor_class 
+    
+    def setFortitude(self):
+        return self.fortitude 
+    
+    def setReflex(self):
+        return self.reflex 
+    
+    def setWillpower(self):
+        return self.willpower 
+    
+    def setCMD(self):
+        return self.cmd 
+    
+    def setHitPoints(self):
+        return self.hit_points 
+    
+    def setMelee(self):
+        return self.melee 
+    
+    def setRanged(self):
+        return self.ranged 
+    
+    def setAttackOptions(self):
+        return self.attack_options 
+    
+    def setCMB(self):
+        return self.cmb 
+    
+    def setStrngth(self):
+        return self.strength 
+    
+    def setDexterity(self):
+        return self.dexterity 
+    
+    def setConstitution(self):
+        return self.constitution 
+    
+    def setIntelligence(self):
+        return self.intelligence 
+    
+    def setWidsome(self):
+        return self.wisdom 
+    
+    def setCharisma(self):
+        return self.charisma 
 
--- models/monster/monster.py
+++ models/monster/monster.py
@@ -32,6 +32,7 @@ class Monster():
         self.setChallengeRating(monster_array.getChallengeRating())
         self.setArmorClass(monster_array.getArmorClass())
         self.setTouchArmorClass(monster_array.getTouchArmorClass())
+        self.setFortitude(monster_array.getFortitude())
 
 
     def setArmorClass(self, armor_class):
@@ -45,3 +46,75 @@ class Monster():
 
     def getChallengeRating(self):
         return self.cr
+
+    def setName(self, name):
+        self.name = name
+
+    def setXP(self, xp):
+        self.xp = xp
+
+    def setMonsterType(self, monster_type):
+        self.monster_type = monster_type
+
+    def setInit(self, init):
+        self.init = init
+
+    def setSenses(self, senses):
+        self.senses = senses
+
+    def setPerception(self, perception):
+        self.perception = perception
+
+    def setSize(self, size):
+        self.size = size
+
+    def setSpeed(self, speed):
+        self.speed = speed
+
+    def setFlatFootedArmorClass(self, ffac):
+        self.flat_footed_armor_class = ffac
+
+    def setFortitude(self, fortitude):
+        self.fortitude = fortitude
+
+    def setReflex(self, reflex):
+        self.reflex = reflex
+
+    def setWillpower(self, willpower):
+        self.willpower = willpower
+
+    def setCMD(self, cmd):
+        self.cmd = cmd
+
+    def setHitPoints(self, hit_points):
+        self.hit_points = hit_points
+
+    def setMelee(self, melee):
+        self.melee = melee
+
+    def setRanged(self, ranged):
+        self.ranged = ranged
+
+    def setAttackOptions(self, attack_options):
+        self.attack_options = attack_options
+
+    def setCMB(self, cmb):
+        self.cmb = cmb
+
+    def setStrngth(self, strength):
+        self.strength = strength
+
+    def setDexterity(self, dexterity):
+        self.dexterity = dexterity
+
+    def setConstitution(self, constitution):
+        self.constitution = constitution
+
+    def setIntelligence(self, intelligence):
+        self.intelligence = intelligence
+
+    def setWidsome(self, widsom):
+        self.wisdom = widsom
+
+    def setCharisma(self, charisma):
+        self.charisma = charisma
